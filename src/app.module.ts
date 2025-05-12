be4ca41022c8b1b0157140a747b1305af5c6b848
Rehan Adi Purwana <rehanadipurwana@gmail.com>
2025-05-12 17:36:43 +0700

    feat: add validation and logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import * as Joi from "joi";
 import { DatabaseModule } from "./common/database/database.module";
 import { GraphQLModule } from "@nestjs/graphql";
 import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { UsersModule } from './users/users.module';
 import { GraphQLCSSMiddleware } from './app.middleware';
+import { LoggerModule } from "nestjs-pino";
 
 @Module({
   imports: [
@@ -20,6 +21,26 @@ import { GraphQLCSSMiddleware } from './app.middleware';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
+    }),
+    LoggerModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        const isProduction = configService.get<string>('NODE_ENV') === 'production';
+
+        return {
+          pinoHttp: {
+            transport: isProduction
+              ? undefined
+              : {
+                target: 'pino-pretty',
+                options: {
+                  singleLine: true,
+                },
+              },
+            level: isProduction ? 'info' : 'debug',
+          },
+        }
+      },
+      inject: [ConfigService],
     }),
     DatabaseModule,
     UsersModule,
