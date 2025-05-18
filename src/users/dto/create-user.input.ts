b08b7ab61e268de178d7ff26aec533836b67bc3c
Rehan Adi Purwana <rehanadipurwana@gmail.com>
2025-05-18 11:42:58 +0700

    feat: add username field

--- src/users/dto/create-user.input.ts
+++ src/users/dto/create-user.input.ts
@@ -1,11 +1,16 @@
 import { InputType, Field } from '@nestjs/graphql';
-import { IsEmail, IsStrongPassword } from "class-validator";
+import { IsEmail, IsNotEmpty, IsString, IsStrongPassword } from "class-validator";
 
 @InputType()
 export class CreateUserInput {
   @Field()
   @IsEmail()
   email: string;
+
+  @Field()
+  @IsString()
+  @IsNotEmpty()
+  username: string;
 
   @Field()
   @IsStrongPassword()
