b51da2ac58031faf980e259653e74721110591f2
Rehan Adi Purwana <rehanadipurwana@gmail.com>
2025-05-21 15:44:37 +0700

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.useLogger(app.get(Logger));
   app.use(cookieParser());
+  app.enableCors();
 
   const configService = app.get(ConfigService);
   await app.listen(configService.getOrThrow('PORT'));
