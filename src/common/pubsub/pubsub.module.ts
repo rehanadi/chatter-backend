7dd78fa1db3afab157bf00daac41488b89dda8a7
Rehan Adi Purwana <rehanadipurwana@gmail.com>
2025-05-22 12:31:23 +0700

    feat: add redis pub sub

--- src/common/pubsub/pubsub.module.ts
+++ src/common/pubsub/pubsub.module.ts
@@ -1,6 +1,10 @@
 import { Global, Module } from "@nestjs/common";
 import { PUB_SUB } from "../constants/injection-tokens";
 import { PubSub } from "graphql-subscriptions";
+import { ConfigService } from "@nestjs/config";
+import { RedisPubSub } from "graphql-redis-subscriptions";
+import Redis from "ioredis";
+import { reviver } from "./reviver.util";
 
 // The @Global() decorator ensures this module is available application-wide without needing to import it in every module that needs pub/sub functionality
 @Global()
@@ -8,7 +12,23 @@ import { PubSub } from "graphql-subscriptions";
   providers: [
     {
       provide: PUB_SUB, // Creates a singleton PubSub instance from the graphql-subscriptions package
-      useValue: new PubSub(),
+      useFactory: (configService: ConfigService) => {
+        if (configService.get("NODE_ENV") === "production") {
+          const options = {
+            host: configService.getOrThrow("REDIS_HOST"),
+            port: configService.getOrThrow("REDIS_PORT"),
+          };
+
+          return new RedisPubSub({
+            publisher: new Redis(options), // Creates a Redis client for publishing messages
+            subscriber: new Redis(options), // Creates a Redis client for subscribing to messages
+            reviver: reviver, // Handle message serialization/deserialization
+          });
+        }
+
+        return new PubSub(); // Creates a local PubSub instance for development/testing
+      },
+      inject: [ConfigService],
     }
   ],
   exports: [PUB_SUB], // Makes the PubSub service available to be injected in other modules
